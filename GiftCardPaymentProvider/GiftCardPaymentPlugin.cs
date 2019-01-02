761d7293348c9eda3a28e51e73e40e3f85ca0ec7
Bruce Gordon <bruce@focusls.com>
2019-01-02 14:45:50 -0500

    1

---
+++ GiftCardPaymentProvider/GiftCardPaymentPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using EPiServer.Commerce.Order;
+
+namespace GiftCardPaymentProvider
+{
+    public class GiftCardPaymentPlugin : IPaymentPlugin
+    {
+        public IDictionary<string, string> Settings { get; set; }
+
+        public PaymentProcessingResult ProcessPayment(IOrderGroup orderGroup, IPayment payment)
+        {
+            return PaymentProcessingResult.CreateSuccessfulResult("Gift card payment accepted!");
+        }
+    }
+}
